b0cc1f588287948ca3bc2c07c2992d7c65170188
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2011-03-28 12:06:16 +0200

    imported already existing extensions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,31 +1,34 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    inherits "global" // inherit Grails' default dependencies
+    log "warn"
+
     repositories {
-        grailsPlugins()
         grailsHome()
         grailsCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        // runtime 'mysql:mysql-connector-java:5.1.13'
+    mavenRepo "https://build.intuitive-collaboration.com/maven/plugins/"
+
+    String ulcVersion = "ria-suite-u2"
+
+    plugins {
+        runtime ":maven-publisher:0.7.5"
+        compile "com.canoo:ulc:${ulcVersion}"
+    }
+}
+
+grails.project.dependency.distribution = {
+    String passPhrase = ""
+    String scpUrl = ""
+    try {
+        Properties properties = new Properties()
+        properties.load(new File("${userHome}/deployInfo.properties").newInputStream())
+
+        passPhrase = properties.get("passPhrase")
+        scpUrl = properties.get("url")
+    } catch (Throwable t) {
+    }
+    remoteRepository(id: "pillarone", url: scpUrl) {
+        authentication username: 'root', privateKey: "${userHome.absolutePath}/.ssh/id_rsa", passphrase: passPhrase
     }
 }
