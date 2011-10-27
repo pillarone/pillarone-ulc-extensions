d4ed8c04dd89e1bd5f52c81d595aaffde3a94c28
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2011-10-27 17:19:16 +0200

    fixed deployment settings

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,18 +44,20 @@ grails.project.dependency.resolution = {
 }
 
 grails.project.dependency.distribution = {
-    String passPhrase = ""
+    String password = ""
+    String user = ""
     String scpUrl = ""
     try {
         Properties properties = new Properties()
         properties.load(new File("${userHome}/deployInfo.properties").newInputStream())
 
-        passPhrase = properties.get("passPhrase")
+        user = properties.get("user")
+        password = properties.get("password")
         scpUrl = properties.get("url")
     } catch (Throwable t) {
     }
     remoteRepository(id: "pillarone", url: scpUrl) {
-        authentication username: 'root', privateKey: "${userHome.absolutePath}/.ssh/id_rsa", passphrase: passPhrase
+        authentication username: user, password: password
     }
 }
 
