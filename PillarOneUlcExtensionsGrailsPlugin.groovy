a0652749da6dad92043e86efa36427584eddd0ba
detlef-brendle <detlef.brendle@canoo.com>
2013-06-18 13:27:30 +0200

    new version

--- PillarOneUlcExtensionsGrailsPlugin.groovy
+++ PillarOneUlcExtensionsGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class PillarOneUlcExtensionsGrailsPlugin {
     // the plugin version
-    def version = "0.7"
+    def version = "0.8"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2.1 > *"
     // the other plugins this plugin depends on
